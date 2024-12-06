f171a677635e3bf66e64ecc93fee7d9b1a78cdf7
SShrey1 <palishrey@gmail.com>
2024-12-06 23:48:19 +0530

    Final

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -26,7 +26,47 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
         {
+            imageView.image = userPickedImage
             
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could Not Convert UIImage into CI Image ")
+            }
+            
+            detect(image: ciImage)
+                
+            
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func detect(image : CIImage){
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+//            print(results)
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog"){
+                    self.navigationItem.title = "HotDog!"
+                } else {
+                    self.navigationItem.title = "Not HotDog!"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
         }
     }
 
