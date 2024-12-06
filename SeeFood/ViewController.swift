725a46d36fc7a29893eaae743bb563a75c79e3c0
SShrey1 <palishrey@gmail.com>
2024-12-06 09:57:09 +0530

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by user@59 on 15/09/1946 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SeeFoodTests/SeeFoodTests.swift
@@ -0,0 +1,17 @@
+//
+//  SeeFoodTests.swift
+//  SeeFoodTests
+//
+//  Created by user@59 on 15/09/1946 Saka.
+//
+
+import Testing
+@testable import SeeFood
+
+struct SeeFoodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
